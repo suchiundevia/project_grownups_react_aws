6182eabf260133e8ea6fa38e2fcc9320bcf3b596
Suchi Undevia <suchi.undevia@gmail.com>
2020-09-07 11:19:26 +1200

    Paths for base app adjusted

--- base_app/views.py
+++ base_app/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, '../base_app/templates/base_app/home.html', {'name': 'Test'})
+    return render(request, 'base_app/home.html', {'name': 'Test'})
 
 
 def about(request):
-    return render(request, '../base_app/templates/base_app/about.html', {'title': 'About'})
+    return render(request, 'base_app/about.html', {'title': 'About'})
