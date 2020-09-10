9e8b36822ced9d34196596d54358ea280f533e3b
Suchi Undevia <suchi.undevia@gmail.com>
2020-09-10 15:16:43 +1200

    SendGrid keys added

--- grownups/settings.py
+++ grownups/settings.py
@@ -170,12 +170,13 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 # Email backend connection
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 
 # Variables for AWS S3 bucket connection
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
