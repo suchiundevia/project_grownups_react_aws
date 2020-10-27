862fcc4111c73289a3f8db8da2a363fc4ddc9853
Suchi Undevia <suchi.undevia@gmail.com>
2020-10-28 08:29:59 +1300

    News dataset added.

--- activity_app/admin.py
+++ activity_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Activity
+from .models import Activity, News, Interest, VisitorNotification
 
 admin.site.register(Activity)
+admin.site.register(News)
+admin.site.register(Interest)
+admin.site.register(VisitorNotification)
